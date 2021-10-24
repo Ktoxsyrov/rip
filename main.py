b024ab33af4d54f44aeff27a40eb2cbddedcb2b4
Ktoxsyrov <ktoxsyrovv@inbox.ru>
2021-10-25 00:16:29 +0300

    done

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from subject_area.detail import Detail
 from subject_area.manufacturer import Manufacturer
-from subject_area.details_prov import DetailsProv
+from subject_area.details_man import DetailsMan
 
 from operator import itemgetter
 
-providers = [
+manufacturers = [
     Manufacturer(1, "Russian Manufacturer", "Russia"),
     Manufacturer(2, "English Manufacturer", "UK"),
     Manufacturer(3, "Chad Manufacturer", "Chad"),
@@ -18,35 +18,35 @@ details = [
 ]
 
 
-details_prov = [
-    DetailsProv(1, 1),
-    DetailsProv(1, 2),
+details_man = [
+    DetailsMan(1, 1),
+    DetailsMan(1, 2),
 
-    DetailsProv(2, 2),
-    DetailsProv(2, 3),
+    DetailsMan(2, 2),
+    DetailsMan(2, 3),
 
-    DetailsProv(3, 3),
-    DetailsProv(3, 1),
+    DetailsMan(3, 3),
+    DetailsMan(3, 1),
 
-    DetailsProv(4, 3),
-    DetailsProv(4, 2)
+    DetailsMan(4, 3),
+    DetailsMan(4, 2)
 ]
 
 def main():
 
-    one_to_many = [(det.name, prov.name, prov.country)
+    one_to_many = [(det.name, man.name, man.country)
                    for det in details
-                   for prov in providers
-                   if det.prov_id == prov.id
+                   for man in manufacturers
+                   if det.man_id == man.id
                    ]
 
-    many_to_many_temp = [(prov.name, dp.prov_id, dp.detail_id)
-                         for prov in providers
-                         for dp in details_prov
-                         if prov.id == dp.prov_id]
+    many_to_many_temp = [(man.name, dp.man_id, dp.detail_id)
+                         for man in manufacturers
+                         for dp in details_man
+                         if man.id == dp.man_id]
 
-    many_to_many = [(det.name, prov_name)
-                    for prov_name, prov_id, detail_id in many_to_many_temp
+    many_to_many = [(det.name, man_name)
+                    for man_name, man_id, detail_id in many_to_many_temp
                     for det in details if det.id == detail_id]
 
     print('\nЗадание Б1')
@@ -56,9 +56,9 @@ def main():
 
     print('\nЗадание Б2')
     task_2 = []
-    for prov in providers:
-        det_prov = list(filter(lambda elem: elem[1] == prov.name, one_to_many))
-        task_2.append([prov.name, len(det_prov)])
+    for man in manufacturers:
+        det_man = list(filter(lambda elem: elem[1] == man.name, one_to_many))
+        task_2.append([man.name, len(det_man)])
     print(sorted(task_2, key=itemgetter(1)))
 
 
@@ -66,9 +66,9 @@ def main():
     task_3 = {}
     for det in details:
         if (det.name.startswith('b') or det.name.startswith('s')):
-            det_prov = list(
+            det_man = list(
                 filter(lambda elem: elem[0] == det.name, many_to_many))
-            task_3[det.name] = [x for _, x in det_prov]
+            task_3[det.name] = [x for _, x in det_man]
     print(task_3)
 
 if __name__ == "__main__":
--- subject_area/detail.py
+++ subject_area/detail.py
@@ -1,6 +1,6 @@
 class Detail:
-    def __init__(self, id, name, material, prov_id):
+    def __init__(self, id, name, material, man_id):
         self.id = id
         self.name = name
         self.material = material
-        self.prov_id = prov_id
+        self.man_id = man_id
